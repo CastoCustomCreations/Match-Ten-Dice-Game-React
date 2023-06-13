0bfeca0a9acb222cc15e1353b56b269946c071bd
Jen Casto <CastoCustomCreations@gmail.com>
2023-06-13 13:57:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TENZIES-GAME-REACT-APP/",
+  base: "/Match-Ten-Dice-Game-React/",
   plugins: [react()],
 });
